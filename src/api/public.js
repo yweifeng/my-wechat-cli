d1591a3b1a02bad2e65345b2f836fcd41a805416
yweifeng <yweifeng0421@sina.com>
2020-06-03 15:34:18 +0800

    代码优化

--- src/api/public.js
+++ src/api/public.js
@@ -7,27 +7,3 @@ import request from "@utils/request";
 export function getHomeData() {
   return request.get("index", {}, { login: false });
 }
-
-/**
- * 获取微信sdk配置
- * @returns {*}
- */
-export function getWechatConfig() {
-  return request.get(
-    "/wechat/config",
-    { url: document.location.href },
-    { login: false }
-  );
-}
-
-/**
- * 获取微信sdk配置
- * @returns {*}
- */
-export function wechatAuth(code, spread, login_type) {
-  return request.get(
-    "/wechat/auth",
-    { code, spread, login_type },
-    { login: false }
-  );
-}
